bcee4c7734286c2e1032f877e315f8b3e564c21f
m.gystarov <m_gystarov@dss-lab.local>
2022-03-09 14:55:27 +0300

    update

--- testing.py
+++ testing.py
@@ -3,27 +3,23 @@ import tensorflow as tf
 import numpy as np
 import editdistance
 from tqdm import tqdm
-from data_prepare import get_words_and_spells, get_tokens, get_x_y
+from data_prepare import get_words_and_spells, get_x_y
+from data.vocabs import GRAPHEMES, PHONEMES
 from graphs.deep_blstm import create_blstm
 
 
-def prepare_test_data(path_to_test_data, path_to_tokens_grapheme, path_to_tokens_phoneme):
+def prepare_test_data(path_to_test_data):
     words, spells = get_words_and_spells(path_to_test_data)
-    tokens_grapheme, tokens_phoneme = get_tokens(path_to_tokens_grapheme, path_to_tokens_phoneme)
-    X, Y = get_x_y(words, spells, tokens_grapheme, tokens_phoneme)
+    X, Y = get_x_y(words, spells, GRAPHEMES, PHONEMES)
     return X, Y
 
 
-def func_testing(model, X, Y, result, path_to_tokens_phoneme):
+def func_testing(model, X, Y, result, PHONEMES):
     """
     вычисляем метрики wer(word error rate) = 1 - acc, cer(character error rate)
     avg_cer - средняя арифметичская(если мы хотим сделать акцент на слове)
     avg_cer_1 - cer посчитанный будто на одну длинную последо-сть.
     """
-    
-    with open(path_to_tokens_phoneme, 'r') as f:
-        lines_2 = f.readlines()
-        tokens_phoneme = [line.strip() for line in lines_2]
     
     total_cer = 0
     total_size = 0
@@ -51,8 +47,8 @@ def func_testing(model, X, Y, result, path_to_tokens_phoneme):
         total_cer += cer
         total_distance += distance
         total_size += len(sequence_true)
-        phonemes_true = decoding_phonemes(sequence_true, tokens_phoneme)
-        phonemes_pred = decoding_phonemes(sequence_pred, tokens_phoneme)
+        phonemes_true = decoding_phonemes(sequence_true, PHONEMES)
+        phonemes_pred = decoding_phonemes(sequence_pred, PHONEMES)
         result.write(f'{phonemes_true}\t{phonemes_pred}\t{cer}\n')
     avg_cer = total_cer / len(Y)
     avg_cer_1 = total_distance / total_size
@@ -72,23 +68,16 @@ def decoding_phonemes(indicies, tokens_phoneme):
 
 
 if __name__ == '__main__':
-    with open('configs/train_config.yaml', 'r') as f:
+    with open('configs/test_config.yaml', 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    with open('configs/deep_blstm.yaml', 'r') as f:
+    with open('models/deep_blstm/config_deep_blstm.yaml', 'r') as f:
         config_model = yaml.load(f, Loader=yaml.FullLoader)
-    model_name = 'g2p_model_deep_blstm'
-    path_load_model = f"{config['path_to_work_dir']}/{model_name}"
-    path_to_test_data = f'{config["path_to_sources"]}/data/test.txt'
-    path_to_tokens_grapheme = f'{config["path_to_sources"]}/data/graphemes.txt'
-    path_to_tokens_phoneme = f'{config["path_to_sources"]}/data/phonemes.txt'
-    path_to_result = f"{config['path_to_work_dir']}/result_{model_name.split('_')[-1]}.txt"
-    checkpoint_path = f'{config["path_to_work_dir"]}/ckpt_{model_name}/best.ckpt'
+    model = create_blstm(config_model)
+    model.load_weights(config['path_checkpoint'])
+    path_to_result = (config['path_to_result_model'])
     result = open(path_to_result, 'w')
-    X, Y = prepare_test_data(path_to_test_data, path_to_tokens_grapheme, path_to_tokens_phoneme)
+    X, Y = prepare_test_data(config['path_test_data'])
     X = X[:100]
     Y = Y[:100]
-    model = create_blstm(config_model)
-    model.load_weights(checkpoint_path)
-    # model = tf.keras.models.load_model(path_load_model)
-    func_testing(model, X, Y, result, path_to_tokens_phoneme)
+    func_testing(model, X, Y, result, PHONEMES)
     result.close()
